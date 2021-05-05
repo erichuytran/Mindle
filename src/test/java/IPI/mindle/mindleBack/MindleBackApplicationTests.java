259c5f0c1eca24db568e2354010b7f8cc418dd3a
marine <dedola.marine@hotmail.com>
2021-05-05 22:48:09 +0200

    Dossier tests

---
+++ src/test/java/IPI/mindle/mindleBack/MindleBackApplicationTests.java
@@ -0,0 +1,13 @@
+package IPI.mindle.mindleBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindleBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
